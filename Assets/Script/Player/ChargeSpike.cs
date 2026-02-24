2a7f5ae03b87286f46b7431f6569759e4fb25649
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-02-25 00:58:10 +0900

    Decalを伸ばす途中

---
+++ Assets/Script/Player/ChargeSpike.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ChargeSpike : MonoBehaviour
+{
+    //[SerializeField] private Renderer targetRender;
+    [SerializeField] private DecalProjector targetRender;
+    [SerializeField] private float chargeSpeed = 1.5f;
+
+    private Material mat;
+    private float charge;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mat = targetRender.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            charge += Time.deltaTime * chargeSpeed;
+        }
+        else
+        {
+            charge -= Time.deltaTime * chargeSpeed;
+        }
+
+        charge = Mathf.Clamp01(charge);
+
+        mat.SetFloat("_Charge", charge);
+    }
+}
